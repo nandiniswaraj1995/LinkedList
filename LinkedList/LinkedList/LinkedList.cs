44431fd0a3bb1633bcdf0bc14714364e67bb94bd
unknown <nandiniswaraj13@gmail.com>
2021-03-25 11:27:39 +0530

    [Nandiniswaraj]Added Add At Particular Position method added for LinkedList

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -39,6 +39,39 @@ namespace LinkedList
                 Console.WriteLine("{0} inserted into the linked list", node.data);
             }
         }
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) 
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+
+
         internal void Display()
         {
             Node temp = this.head;
@@ -50,7 +83,7 @@ namespace LinkedList
             while (temp != null)
             {
                 Console.Write(temp.data + " ");
-                temp = temp.next; //temp=null
+                temp = temp.next;
             }
         }
 
@@ -66,6 +99,7 @@ namespace LinkedList
             list.AddAtFirst(70);
             list.AddAtFirst(30);
             list.AddAtFirst(56);
+            list.InsertAtParticularPosition(2, 20);
             list.Display();
 
 
