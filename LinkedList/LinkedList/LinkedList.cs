bf6dff71112bd2ea728b8e2148c9910076b66e00
unknown <nandiniswaraj13@gmail.com>
2021-03-25 11:21:34 +0530

    [Nandiniswaraj]Added Add At First Position method added for LinkedList

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -23,16 +23,51 @@ namespace LinkedList
             }
             Console.WriteLine("{0} inserted into the linked list", node.data);
         }
+        internal void AddAtFirst(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+                Console.WriteLine("{0} inserted into the linked list", node.data);
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+
+                Console.WriteLine("{0} inserted into the linked list", node.data);
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+
 
 
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
 
-           list.Add(70);
+            list.Add(70);
             list.Add(30);
             list.Add(56);
-           
+            list.AddAtFirst(70);
+            list.AddAtFirst(30);
+            list.AddAtFirst(56);
+            list.Display();
+
 
         }
     }
