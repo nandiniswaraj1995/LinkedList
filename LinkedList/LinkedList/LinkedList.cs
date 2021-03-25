07bbdcc5c4ac2d9c4bea6d63321568c6125b3b23
unknown <nandiniswaraj13@gmail.com>
2021-03-25 11:09:06 +0530

    [Nandiniswaraj]Added Add method added for LinkedList

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -7,17 +7,32 @@ namespace LinkedList
 
 
         internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
 
 
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
 
-            list.head = new Node(56);
-            Node second = new Node(30);
-            Node third = new Node(70);
-            list.head.next = second;
-            second.next = third;
+           list.Add(70);
+            list.Add(30);
+            list.Add(56);
+           
 
         }
     }
