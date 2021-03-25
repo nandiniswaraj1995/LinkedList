a020a0595b95213f2e9244d8cc93eefb38be50c2
unknown <nandiniswaraj13@gmail.com>
2021-03-25 11:01:00 +0530

    [Nandiniswaraj]Added Simple linked list with 3 nodes added

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+
+
+        internal Node head;
+
+
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            list.head = new Node(56);
+            Node second = new Node(30);
+            Node third = new Node(70);
+            list.head.next = second;
+            second.next = third;
+
+        }
+    }
+}
